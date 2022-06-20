aa52e03b61bf08e370c1fc024dcdb8f631aaac48
Soujanya guntuku <soujanya.guntuku@Loaner-Mac-FBU-C02SY1C0GTF1.local>
2022-06-20 07:35:30 +0200

    inheritance, overroding and 2d Arrays

---
+++ src/main/java/com/td/ca/javalearn/Inheritance/Pho.java
@@ -0,0 +1,7 @@
+package com.td.ca.javalearn.Inheritance;
+
+public class Pho extends Noodle{
+  public Pho(){
+    super(30.0,0.64,"flat","rice flour");
+  }
+}
---
+++ src/main/java/com/td/ca/javalearn/Inheritance/Spaghetti.java
@@ -0,0 +1,8 @@
+package com.td.ca.javalearn.Inheritance;
+
+public class Spaghetti extends Noodle{
+
+  Spaghetti(double lenInCent, double wthInCent, String shp, String ingr) {
+    super(lenInCent, wthInCent, shp, ingr);
+  }
+}
